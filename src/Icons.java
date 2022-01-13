27c731de9618721b4717ea571da265075d38d140
GabrielLopez <troyasoft1642@gmail.com>
2022-01-13 15:19:07 -0600

    primer version funcional

---
+++ src/Icons.java
@@ -0,0 +1,6 @@
+import javax.swing.*;
+
+public interface Icons {
+    ImageIcon MARIO = new ImageIcon(Icons.class.getResource("/finite_walk.gif"));
+    ImageIcon SHELL = new ImageIcon(Icons.class.getResource("/infinito.gif"));
+}
